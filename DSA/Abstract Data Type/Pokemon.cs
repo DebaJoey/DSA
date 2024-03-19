4be6e0c34dcbc1d8d39146dd134b27068b77149c
Joshua Debazar <debajoshua@gmail.com>
2024-03-19 13:52:59 +0000

    Initial Commit

---
+++ DSA/Abstract Data Type/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DSA.AbstractDataType
+{
+	public class Pokemon
+	{
+		public string Name { get; set; }
+
+		public int Exp { get; set; }
+
+
+
+		public bool isPokemon()
+		{
+			return true;
+		}
+
+	}
+
+}
---
+++ DSA/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DSA.AbstractDataType;
+
+Pokemon squirtle = new Pokemon();
+
+Console.WriteLine(squirtle.isPokemon());
+

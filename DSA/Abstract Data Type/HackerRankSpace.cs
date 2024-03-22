beb5f9827a0b4654b53c5603cde997d00fe961e7
Joshua Debazar <debajoshua@gmail.com>
2024-03-22 00:55:28 +0000

    MarsClass

---
+++ DSA/Abstract Data Type/HackerRankSpace.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace DSA.AbstractDataType
+{
+	public class HackerRankSpace
+	{
+
+        public static int marsExploration(string s)
+        {
+
+            string message = "SOS";
+            int messageLength = s.Length/3;
+
+            string expectedMessage = string.Concat(Enumerable.Repeat(message, messageLength));
+
+
+
+            int finalLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != expectedMessage[i])
+                {
+                    finalLength++;
+                }
+            }
+
+            return finalLength;
+
+        }
+
+    }
+}
+

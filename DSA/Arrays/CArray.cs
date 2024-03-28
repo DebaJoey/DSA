296eafecfdf77d81ef3a9c23b9bcb2732cf81002
Joshua Debazar <debajoshua@gmail.com>
2024-03-28 11:31:35 +0000

    CArray class implemented

---
+++ DSA/Arrays/CArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DSA.Arrays
+{
+	public class CArray
+	{
+		private int[] arr;
+		private int upper;
+		private int numElements;
+
+
+		public CArray(int size)
+		{
+			arr = new int[size];
+			upper = size - 1;
+			numElements = 0;
+		}
+
+
+		public void Insert(int item)
+		{
+			arr[numElements] = item;
+			numElements++;
+		}
+
+		public void DisplayElements()
+		{
+			for(int i = 0; i <= upper; i++ )
+			{
+				Console.WriteLine(arr[i] + " ");
+			}
+		}
+
+		public void Clear()
+		{
+			for(int i = 0; i <= upper; i++)
+			{
+				arr[i] = 0;
+				numElements = 0;
+			}
+		}
+	}
+}
+
